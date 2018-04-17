8732a4ed33a941fdd54324616ad24e5f52822ea7
Fabien RAJAONARISON <narajaon@student.42.fr>
2018-04-17 19:24:36 +0200

    Trying to fix vertical align of sViews

---
+++ Piscine/calculatrice/calculatrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculatrice
+//
+//  Created by RAJAONARISON on 17/04/2018.
+//  Copyright © 2018 narajaon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
