eac1ea8593e36178beee15d7ff2b3f56302db6ac
Fabien RAJAONARISON <narajaon@student.42.fr>
2018-04-20 19:22:16 +0200

    Debugging a crash when a numeric button is pressed

--- Piscine/calculatrice/calculatrice/ViewController.swift
+++ Piscine/calculatrice/calculatrice/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printedInputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func outputPressedButton(_ sender: UIButton) {
+        let concatOutput = printedInputLabel.text
+        var finalOutput: String
+        
+        if (concatOutput == "0") {
+            printedInputLabel.text = sender.currentTitle
+            return
+        }
+        
+        finalOutput = sender.currentTitle!
+        finalOutput += concatOutput!
+        printedInputLabel.text = finalOutput
+    }
+    
+    @IBAction func formatOuputNumberr(_ sender: UIButton) {
+        var negativNumber: String
+        
+        negativNumber = "-"
+        if (sender.currentTitle == "AC") {
+            printedInputLabel.text = "0"
+        }
+        else if (sender.currentTitle == "NEG"){
+            negativNumber += printedInputLabel.text!
+            printedInputLabel.text = negativNumber
+        }
+    }
 }
 
