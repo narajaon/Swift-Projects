4e80fcf6be138e3cfd1d71a27df11bfd2052a1c4
Fabien RAJAONARISON <narajaon@student.42.fr>
2018-04-20 10:35:26 +0200

    trying to understand how memory is handled in swift

---
+++ Piscine/foodTracker/foodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by RAJAONARISON on 19/04/2018.
+//  Copyright © 2018 narajaon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
