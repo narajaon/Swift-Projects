40d059aa8cecd386705aec5a67c9bc02cc25cf67
Fabien RAJAONARISON <narajaon@student.42.fr>
2018-04-23 10:37:58 +0200

    d00 done

--- Piscine/calculatrice/calculatrice/ViewController.swift
+++ Piscine/calculatrice/calculatrice/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+func doOperation(_ a: Int, _ b: Int, _ oper: String) -> Int {
+    switch oper {
+    case "+" :
+        return a &+ b
+    case "-" :
+        return a &- b
+    case "*" :
+        return a &* b
+    case "/" :
+        return a / b
+    default :
+        return 0
+    }
+}
 
+struct OperStruct {
+    var previousValue = 0
+    var oper: String?
+}
+
+class ViewController: UIViewController {
+    var currentOper = OperStruct()
+    var isInputMode = false
     @IBOutlet weak var printedInputLabel: UILabel!
     
     override func viewDidLoad() {
@@ -23,16 +44,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func outputPressedButton(_ sender: UIButton) {
-        let concatOutput = printedInputLabel.text
+        let concatOutput = printedInputLabel.text!
         var finalOutput: String
         
-        if (concatOutput == "0") {
+        if (concatOutput == "0" || isInputMode == false) {
             printedInputLabel.text = sender.currentTitle
+            isInputMode = true
             return
         }
         
         finalOutput = sender.currentTitle!
-        finalOutput = concatOutput! + finalOutput
+        finalOutput = concatOutput + finalOutput
         printedInputLabel.text = finalOutput
     }
     
@@ -42,11 +64,36 @@ class ViewController: UIViewController {
         negativNumber = "-"
         if (sender.currentTitle == "AC") {
             printedInputLabel.text = "0"
+            currentOper.previousValue = 0
+            currentOper.oper = nil
         }
-        else if (sender.currentTitle == "NEG"){
+        else if (sender.currentTitle == "NEG" && printedInputLabel.text != "0"){
             negativNumber += printedInputLabel.text!
             printedInputLabel.text = negativNumber
         }
     }
-}
 
+    @IBAction func doOperationButton(_ sender: UIButton) {
+        let currentValue = Int(printedInputLabel.text!)
+        let result: Int
+        
+        if (currentOper.oper != nil) {
+            if (currentOper.oper == "/" && currentValue == 0) {
+                // Handle floating point exception
+                result = 0
+            } else {
+                result = doOperation(currentOper.previousValue, currentValue!, currentOper.oper!)
+            }
+            printedInputLabel.text = String(result)
+            currentOper.previousValue = result
+        }
+        else {
+
+            currentOper.previousValue = currentValue!
+        }
+        if (sender.currentTitle != "=") {
+            currentOper.oper = sender.currentTitle
+        }
+        isInputMode = false
+    }
+}
