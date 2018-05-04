3e3c6369d293f85e7a8d9a1ce931c16cbbeed1b8
Fabien RAJAONARISON <narajaon@student.42.fr>
2018-05-04 17:47:40 +0200

    need to fix tableview(cellForRowAt) method to add victimList

--- deathNote/deathNote/deathNote/ViewController.swift
+++ deathNote/deathNote/deathNote/ViewController.swift
@@ -2,38 +2,44 @@
 //  ViewController.swift
 //  deathNote
 //
-//  Created by RAJAONARISON on 26/04/2018.
+//  Created by RAJAONARISON on 04/05/2018.
 //  Copyright © 2018 narajaon. All rights reserved.
 //
 
 import UIKit
 
-func addDefaultCell() {
-    if (VictimList.count == 0) {
-        let defaultCell = Victim(name: "Placeholder", desc: "RIP", date: "Friday 13th")
-
-        VictimList.append(defaultCell)
+class Victim {
+    var name: String
+    var desc: String
+    var death: String
+    
+    init (name: String, desc: String, death: String) {
+        self.name = name
+        self.desc = desc
+        self.death = death
     }
 }
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+let VictimList : [Victim] = [
+    Victim(name: "vict1", desc: "death1", death: "date1"),
+    Victim(name: "vict2", desc: "death2", death: "date2"),
+    Victim(name: "vict3", desc: "death3", death: "date3")
+]
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        addDefaultCell()
         
-        return VictimList.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "victimCell") as! VictimTableViewCell
-
-        cell.victimClass = VictimList[indexPath.row]
-
-        return cell
+        return 1
     }
     
-    @IBAction func unWindSegue(segue : UIStoryboardSegue) {
-        print(segue.identifier ?? "No title")
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "victimCell")
+        
+        return cell!
     }
+    
+
 }
+
